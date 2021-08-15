5d2d80d1433fd836ec811fdb8d3abccbd4d6ff36
mukhtaryasin <mayasin252@gmail.com>
2021-08-15 12:01:35 +1000

    first commit

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const toggleSlider = document.getElementById('slider')
+
+toggleSlider.addEventListener('click', function () {
+  // Toggle between light and dark theme
+  if (localStorage.getItem('theme') === 'theme-light') {
+    setTheme('theme-dark')
+  } else {
+    setTheme('theme-light')
+  }
+})
+
+// Function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName)
+  document.documentElement.className = themeName
+}
+
+// Immediately invoked function to set the theme on initial load
+;(function () {
+  if (localStorage.getItem('theme') === 'theme-light') {
+    setTheme('theme-light')
+    document.getElementById('slider').checked = false
+  } else {
+    setTheme('theme-dark')
+    document.getElementById('slider').checked = true
+  }
+})()
